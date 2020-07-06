25c2a96deed618b442e487dcf8725832ff4545a5
Brett Saxon <brettcsaxon@gmail.com>
2020-07-06 12:20:50 -0400

    actual build

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,7 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
         <Link
-          to="aboutPage"
+          to="about"
           style={{
             color: `white`,
             textDecoration: `none`,
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const AboutPage = () => (
+const about = () => (
   <Layout>
     <SEO title="About" />
-    
-      <h1>Hi from About Page</h1>
-      <p>This is the about page</p>
-    
+
+    <h1>Hi from About Page</h1>
+    <p>This is the about page</p>
   </Layout>
 )
 
-export default AboutPage
+export default about
